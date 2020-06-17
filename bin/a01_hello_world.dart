d0dd8a0d463768398c40584033d42d5f240528ae
Artnalyze <artnalyze@gmail.com>
2020-06-17 21:19:47 +0700

    01 : Hello Dart

---
+++ bin/a01_hello_world.dart
@@ -0,0 +1,15 @@
+// ต้องเริ่มการทำงานจาก method main() เสมอ
+
+void main() {
+  print("Hello World Dart!!");
+
+  // concat string
+  print("I love" + " dart programming");
+  print("My name is Art\nI'm a developer.");
+  print('''Hello to learn dart faster 
+  way in this class
+  and go to production
+  in tomorrow''');
+
+  print(r'This is \n multiline');
+}
